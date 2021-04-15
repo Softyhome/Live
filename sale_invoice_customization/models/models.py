d1e458d80f4e0205a42d97af49c900bf42636d95
Usman Farzand <usman_farzand@outlook.com>
2021-04-15 17:02:15 +0500

    first commit

---
+++ sale_invoice_customization/models/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.model
+    def _default_warehouse_id(self):
+        # !!! Any change to the default value may have to be repercuted
+        # on _init_column() below.
+        return False
+
+    warehouse_id = fields.Many2one(
+        'stock.warehouse', string='Warehouse',
+        required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+        default=_default_warehouse_id, check_company=True)
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account',
+                                          index=True, compute="_compute_analytic_account", store=True, readonly=False,
+                                          check_company=True, copy=True,required=True)
