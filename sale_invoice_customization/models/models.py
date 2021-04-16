6c6d81307db442b0e909bc25523468636eb3a8e2
Usman Farzand <usman_farzand@outlook.com>
2021-04-16 23:12:32 +0500

    fix

--- sale_invoice_customization/models/models.py
+++ sale_invoice_customization/models/models.py
@@ -13,7 +13,18 @@ class SaleOrder(models.Model):
     warehouse_id = fields.Many2one(
         'stock.warehouse', string='Warehouse',
         required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-        default=False, check_company=True)
+        default=_default_warehouse_id, check_company=True)
+
+    @api.onchange('company_id')
+    def _onchange_company_id(self):
+        if self.company_id:
+            warehouse_id = self.env['ir.default'].get_model_defaults('sale.order').get('warehouse_id')
+            self.warehouse_id = False  # warehouse_id or self.user_id.with_company(self.company_id.id)._get_default_warehouse_id().id
+
+    @api.onchange('user_id')
+    def onchange_user_id(self):
+        super().onchange_user_id()
+        self.warehouse_id = False  # self.user_id.with_company(self.company_id.id)._get_default_warehouse_id().id
 
 
 class AccountMoveLine(models.Model):
