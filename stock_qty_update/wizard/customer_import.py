1be7ce541797d2d7d648049091c468610cd971f0
ayush <ayush@iqminds.org>
2021-05-30 16:59:26 +0530

    add changes

--- stock_qty_update/wizard/customer_import.py
+++ stock_qty_update/wizard/customer_import.py
@@ -15,32 +15,52 @@ class StockImport(models.TransientModel):
         wb = xlrd.open_workbook(file_contents=base64.decodestring(self.file))
         sheet = wb.sheet_by_index(0)
         sheet.cell_value(0, 0)
-        product = self.env['product.product']
         data = [sheet.row_values(rowx) for rowx in range(1, sheet.nrows)]
+        line = []
+        product_list = []
         for r in data:
             if r[0]:
-                product_rec = self.env['product.product'].search([('default_code','=',r[0])])
-                stock_quant_rec = self.env['stock.quant'].sudo().create({
-                                'product_id': product_rec.id,
-                                'location_id': self.env['stock.warehouse'].search([('name','=','CPT')],limit=1).lot_stock_id.id,
-                                'inventory_quantity': r[2],
-                                'in_date': datetime.now()-timedelta(days=38),
-                                'quantity': r[2]
-                            })
+                product_rec = self.env['product.product'].search([('default_code', '=', r[0])])
+                product_list.append(product_rec.id)
+                line.append((0, 0, {
+                    'product_id': product_rec.id,
+                    'product_qty': r[2],
+                    'location_id': self.env['stock.warehouse'].search([('name', '=', 'CPT')], limit=1).lot_stock_id.id,
+                }))
+        loc = self.env['stock.warehouse'].search([('name', '=', 'CPT')], limit=1).lot_stock_id
+        stock_inventory_rec = self.env['stock.inventory'].sudo().create({
+            'product_ids': [(6, 0, product_list)],
+            'location_ids': [(6, 0, loc.ids)],
+            'accounting_date': datetime.now().date() - timedelta(days=38),
+            'name': 'CPTWH STOCK ' + str(datetime.now().date() - timedelta(days=38)),
+            'line_ids': line
+        })
+        stock_inventory_rec.action_start()
+        stock_inventory_rec.action_validate()
 
     def create_stock_jhbwh(self):
         wb = xlrd.open_workbook(file_contents=base64.decodestring(self.file))
         sheet = wb.sheet_by_index(1)
         sheet.cell_value(0, 0)
-        product = self.env['product.product']
         data = [sheet.row_values(rowx) for rowx in range(1, sheet.nrows)]
+        line = []
+        product_list = []
         for r in data:
             if r[0]:
-                product_rec = self.env['product.product'].search([('default_code','=',r[0])])
-                stock_quant_rec = self.env['stock.quant'].sudo().create({
-                                'product_id': product_rec.id,
-                                'location_id': self.env['stock.warehouse'].search([('name','=','JHB')],limit=1).lot_stock_id.id,
-                                'inventory_quantity': r[2],
-                                'in_date': datetime.now()-timedelta(days=38),
-                                'quantity': r[2]
-                            })
+                product_rec = self.env['product.product'].search([('default_code', '=', r[0])])
+                product_list.append(product_rec.id)
+                line.append((0, 0, {
+                    'product_id': product_rec.id,
+                    'product_qty': r[2],
+                    'location_id': self.env['stock.warehouse'].search([('name', '=', 'JHB')], limit=1).lot_stock_id.id,
+                }))
+        loc = self.env['stock.warehouse'].search([('name', '=', 'JHB')], limit=1).lot_stock_id
+        stock_inventory_rec = self.env['stock.inventory'].sudo().create({
+            'product_ids': [(6, 0, product_list)],
+            'location_ids': [(6, 0, loc.ids)],
+            'accounting_date': datetime.now().date() - timedelta(days=38),
+            'name': 'JHBWH STOCK ' + str(datetime.now().date() - timedelta(days=38)),
+            'line_ids': line
+        })
+        stock_inventory_rec.action_start()
+        stock_inventory_rec.action_validate()
